66ad468707eb308d19e6acc9f30c872d55ebaa63
Vinicius-hora <vinicius_hora@live.com>
2025-01-16 12:15:22 -0300

    spring-admin

--- src/test/java/com/desafio/pedidos/PedidosApplicationTests.java
+++ src/test/java/com/desafio/pedidos/PedidosApplicationTests.java
@@ -2,8 +2,10 @@ package com.desafio.pedidos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PedidosApplicationTests {
 
 	@Test
